daaa7913e6558038beb8194ad3aebe1a946aed58
G Locke <jkeys726@gmail.com>
2026-04-21 16:27:45 -0500

    worked on questions 1, 2, 3, 5, and 7

--- marsh-data.py
+++ marsh-data.py
@@ -12,11 +12,10 @@ def get_heights_pairwise(frame):
 
     return male_female_pairs
 
-def get_heights(dataframe, column):
+def get_heights(dataframe, column, conversion_function=lambda x: x):
     heights = []
     for row in range(2, dataframe.max_row):
-        heights.append(dataframe[row][column].value)
-
+        heights.append(conversion_function(dataframe[row][column].value))
     return heights
 
 
@@ -24,11 +23,40 @@ def get_heights(dataframe, column):
 marsh_data = openpyxl.load_workbook("heights-data-marsh-1988-great-britain.xlsx")
 marsh_active_sheet = marsh_data.active
 
+galton_data = openpyxl.load_workbook("GaltonFamilies-1886.xlsx")
+galton_sheet = galton_data.active
+
 male_column = 0
 female_column = 1
 
-male_heights = get_heights(marsh_active_sheet, male_column)
-female_heights = get_heights(marsh_active_sheet, female_column)
+marsh_male_heights = get_heights(marsh_active_sheet, male_column)
+marsh_female_heights = get_heights(marsh_active_sheet, female_column)
 
-print(f"Mean male height: {np.mean(male_heights):.3f} mm")
-print(f"Mean female height: {np.mean(female_heights):.3f} mm")
+galton_male_heights = get_heights(galton_sheet, male_column, lambda x: 25.4*x)
+galton_female_heights = get_heights(galton_sheet, female_column, lambda x: 25.4*x)
+
+marsh_male_heights_mean = np.mean(marsh_male_heights)
+marsh_female_heights_mean = np.mean(marsh_female_heights)
+galton_male_heights_mean = np.mean(galton_male_heights)
+galton_female_heights_mean = np.mean(galton_female_heights)
+
+
+# Mean
+print(f"Marsh Mean male height: {marsh_male_heights_mean:.3f} mm")
+print(f"Marsh Mean female height: {marsh_female_heights_mean:.3f} mm")
+print(f"Galton Mean male height: {galton_male_heights_mean:.3f} mm")
+print(f"Galton Mean female height: {galton_female_heights_mean:.3f} mm")
+print(f"marsh x1-x2 = {marsh_male_heights_mean - marsh_female_heights_mean:.3f} mm")
+print(f"galton x1-x2 = {galton_male_heights_mean - galton_female_heights_mean:.3f} mm")
+
+# Standard deviation
+print()
+print(f"Marsh male height standard deviation: {np.std(marsh_male_heights):.3f} mm")
+print(f"Marsh female height standard deviation: {np.std(marsh_female_heights):.3f} mm")
+print(f"Galton male height standard deviation: {np.std(galton_male_heights):.3f} mm")
+print(f"Galton female height standard deviation: {np.std(galton_female_heights):.3f} mm")
+
+# correlation
+print()
+print(f"Correlation between male and female heights (Marsh) {np.corrcoef(marsh_male_heights, marsh_female_heights)[0, 1]:.3f}")
+print(f"Correlation between male and female heights (Galton) {np.corrcoef(galton_male_heights, galton_female_heights)[0, 1]:.3f}")
